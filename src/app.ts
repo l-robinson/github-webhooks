b1691d28361b746d97dfa1ca320d320a7dcedf92
l-robinson <24287902+l-robinson@users.noreply.github.com>
2022-07-29 15:09:21 +0930

    Starting some git operations

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
-const config = require('config');
-const http = require('http');
-const { Webhooks, createNodeMiddleware } = require('@octokit/webhooks');
+import config from 'config';
+import * as http from 'http';
+import { Webhooks, createNodeMiddleware } from '@octokit/webhooks';
+import { PushHandler } from './pushHandler';
+
 const webhooks = new Webhooks({
   secret: config.get('webhook.secret'),
 });
 const hostname = config.get('listen.hostname');
 const port = config.get('listen.port');
+const pushHandler = new PushHandler();
 
-webhooks.on('push', (event) => {
-    console.log(`${event}`);
-});
+webhooks.on('push', pushHandler.onEvent);
 
 http.createServer(createNodeMiddleware(webhooks)).listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
---
+++ src/pushHandler.ts
@@ -0,0 +1,38 @@
+import config from 'config';
+import { EmitterWebhookEvent } from '@octokit/webhooks';
+import { PushEvent } from '@octokit/webhooks-types';
+import * as fs from 'fs';
+import { simpleGit } from 'simple-git';
+
+export class PushHandler {
+  onEvent(event: EmitterWebhookEvent): void {
+    const pushEvent = event.payload as PushEvent;
+    const project = pushEvent.repository.name;
+    const url = pushEvent.repository.clone_url;
+    const path = config.get('app.root') + '/' + project;
+
+    if (!fs.existsSync(path)) {
+      // Clone the repo for the first time
+      console.log(`Cloning ${url} to: ${path}`);
+      simpleGit()
+        .clone(url, path)
+        .then((s) => console.log(`Clone Done ${s}`))
+        .catch((e) => console.log(`Clone Error ${e}`));
+    } else {
+      // Pull the latest code
+      console.log(`Pulling latest code from ${url} to: ${path}`);
+      simpleGit(path)
+        .pull()
+        .then((r) => console.log(`Pull Done ${JSON.stringify(r)}`))
+        .catch((e) => console.log(`Pull Error ${e}`));
+    }
+
+    const language = pushEvent.repository.language;
+    switch (language) {
+      case 'nodejs':
+      case 'java':
+      default:
+        console.log(`Unhandled language: ${language}`);
+    }
+  }
+}
